8095f94d7bb9683a4faf4f946f334ea757a56e3d
ChenQi <1163646727@qq.com>
2020-04-30 18:55:36 +0800

    String类相关测试

---
+++ src/testss/java/lang/StringTest.java
@@ -0,0 +1,12 @@
+package testss.java.lang;
+
+/**
+ * className: StringTest <BR>
+ * description: String类相关测试<BR>
+ * remark: <BR>
+ * auther: ChenQi <BR>
+ * date: 2020/4/30 15:37 <BR>
+ * version 1.0 jdk1.8 <BR>
+ */
+public class StringTest {
+}
